276a9c8bac116681d590b3a82887450d3a53061e
Ernesto Alamilla <gifted.orphan@gmail.com>
2017-03-16 23:04:50 -0400

    Fixed table row-click error when clicking table head

--- app/assets/javascripts/table_tr_click.js
+++ app/assets/javascripts/table_tr_click.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function () {
   // Make table rows clickable
-  $(".table tbody>tr").on('click', function(event) {
+  $(".table tbody>tr.clickable").on('click', function(event) {
     var target = $(event.target);
     if (target.is(":not(a)")) {
       window.location = $(this).data("link")
