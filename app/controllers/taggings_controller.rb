b49140d6f96c8453a4835805dfed02b91b5cc5cc
Ernesto Alamilla <gifted.orphan@gmail.com>
2017-03-16 23:12:53 -0400

    Updated taggings methods

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -1,4 +1,5 @@
 class TaggingsController < BaseController
+  before_action :set_tagging, only: :destroy
 
   respond_to :html, :json
 
@@ -6,21 +7,19 @@ class TaggingsController < BaseController
     @tagging = Tagging.new tagging_params
 
     if @tagging.valid? and @tagging.save
-      flash[:notice] = "Tag was successfully added"
+      flash[:success] = 'Tag was successfully linked.'
     else
-      flash[:error] = "There was a problem with your request"
+      flash[:danger] = 'There was a problem with your request.'
     end
 
     respond_with @tagging, location: nil
   end
 
   def destroy
-    @tagging = Tagging.find params[:id]
-
     if @tagging.destroy
-      flash[:notice] = "Tag was successfully removed"
+      flash[:success] = 'Tag was successfully unlinked.'
     else
-      flash[:error] = "There was a problem with your request"
+      flash[:danger] = 'There was a problem with your request.'
     end
 
     respond_with @tagging, location: nil
@@ -28,7 +27,7 @@ class TaggingsController < BaseController
 
   private
     def set_tagging
-      @tagging = Tagging.find(params[:id])
+      @tagging = current_item.taggings.where(tag_id: params[:tagging][:tag_id]).first
     end
 
     def tagging_params
